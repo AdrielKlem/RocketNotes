0129118db27b09c1d3a787c17291fee12a605a4c
Adriel Klem <adrielklem16@gmail.com>
2023-09-06 18:47:04 -0300

    add components tag

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,6 +3,7 @@ import { Container, Links } from "./styles"
 import { Button } from "../../components/Button"
 import { Header } from "../../components/Header"
 import { Section } from "../../components/Section"
+import { Tag } from "../../components/Tag"
 
 
 export function Details() {
@@ -15,6 +16,11 @@ export function Details() {
         <li><a href="#">https:/rocketseat.com.br</a></li>
         <li><a href="#">https:/rocketseat.com.br</a></li>
       </Links>
+    </Section>
+
+    <Section title="Marcadores">
+        <Tag title="express" />
+        <Tag title="node" />
     </Section>
 
     <Button title="Voltar"/>
