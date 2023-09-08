49018244df5e4f70a69abf410eb1da3e20e579d4
Adriel Klem <adrielklem16@gmail.com>
2023-09-08 20:20:58 -0300

    create form signin

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,9 +1,34 @@
-import { Container } from "./styles";
+import { Container, Form } from "./styles";
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi'
+
+import { Input } from "../../components/Input"
+import { Button } from "../../components/Button"
 
 export function SignIn() {
     return (
         <Container>
-            
+            <Form>
+                <h1>Rocket Notes</h1>
+                <p>Aplicação para salvar e gerenciar seus links úteis.</p>
+
+                <h2>Faça seu login</h2>
+                
+                <Input 
+                    placeholder="E-mail"
+                    type="text"
+                    icon={FiMail}
+                />
+
+                <Input 
+                    placeholder="Senha"
+                    type="password"
+                    icon={FiLock}
+                />
+
+                <Button title="Entrar" />
+
+                <a href="http://" target="_blank" rel="noopener noreferrer">Criar Conta</a>
+            </Form>
         </Container>
     )
 }
