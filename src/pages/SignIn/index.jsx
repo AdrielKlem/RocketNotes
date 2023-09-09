3537f81b84dd2fa7d3a96f960377d836d240778e
Adriel Klem <adrielklem16@gmail.com>
2023-09-09 18:39:42 -0300

    add navegation page

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,5 +1,6 @@
 import { Container, Form, Background } from "./styles";
-import { FiLogIn, FiMail, FiLock } from 'react-icons/fi'
+import { FiMail, FiLock } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
 
 import { Input } from "../../components/Input"
 import { Button } from "../../components/Button"
@@ -27,7 +28,9 @@ export function SignIn() {
 
                 <Button title="Entrar" />
 
-                <a href="http://" target="_blank" rel="noopener noreferrer">Criar Conta</a>
+                <Link to="/register">
+                    Criar conta
+                </Link>
             </Form>
 
             <Background />
